9f6e4043435ffb1019dce5f10b4066dcde42ee2f
Xiu Mei Wu <wuxium@sheridancollege.ca>
2023-05-16 13:59:38 -0400

    First Time commit

---
+++ src/arraydemo/ArrayDemo.java
@@ -0,0 +1,27 @@
+
+package arraydemo;
+
+
+public class ArrayDemo {
+
+    
+    public static void main(String[] args) {
+        
+        /*char[] mylist = new char[5];
+        mylist[0]='R';
+        mylist[1]='o';
+        mylist[2]='c';
+        mylist[3]='o';
+        mylist[4]='k';
+        
+     
+        for (int i = 0; i < mylist.length; i++) {
+            System.out.print(mylist[i]);
+        }*/
+        
+        
+    }
+        
+        
+    
+}
